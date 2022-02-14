3a39acfa71a52197f36095e7b16f305493defc3a
Kavitha Varsha <99395840+elegant-data-profiler@users.noreply.github.com>
2022-02-15 01:28:43 +0530

    Add files via upload

---
+++ def() in class.py
@@ -0,0 +1,9 @@
+class festival:
+	def __init__(self,name,festival):
+		self.name=name
+		self.festival=festival
+		print("Happy Diwali")
+	def my_festival(s):
+		print("Happy"+" "+s.festival+" "+"with"+" "+s.name)
+d=festival("gold sparklers","diwali")
+d.my_festival()
