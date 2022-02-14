38c3f516a37e078dcc6db4bbbb723c6b2bd40e2c
Kavitha Varsha <99395840+elegant-data-profiler@users.noreply.github.com>
2022-02-15 01:26:30 +0530

    Add files via upload

---
+++ change value in class.py
@@ -0,0 +1,10 @@
+class student:
+	def __init__(self,name,rank):
+		self.name=name
+		self.rank=rank
+		print("my rank")
+	def my_student(s):
+		print("my"+" "+s.name+" "+"my"+" "+s.rank)
+d=student('kavitha',1)
+d.rank=2
+print(d.rank)
